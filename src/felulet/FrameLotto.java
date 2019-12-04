a165ff342e4d951e4d9ca2c58b40bba5197fe670
Diak <Diak@10.4.117.19>
2019-12-04 14:30:15 +0100

    added gomb :D

--- src/felulet/FrameLotto.java
+++ src/felulet/FrameLotto.java
@@ -19,16 +19,26 @@ public class FrameLotto extends JFrame{
     
     private void initComponents(){
         this.setTitle("Lottó");
-        this.setSize(400,350);
+        this.setSize(300,400);
         this.setLocationRelativeTo(null);
         
         this.panelMain = (JPanel)this.getContentPane();
         this.panelMain.setLayout(null);
+        
         this.txtField = new JTextField();
         this.txtField.setLocation(20,20);
         this.txtField.setSize(250,20);
         this.txtField.setText("Sajt uwu");
+
+        this.buttonOk = new JButton();
+        this.buttonOk.setText("Sorsol");
+        this.buttonOk.setLocation(20,50);
+        this.buttonOk.setSize(250,25);
+
         this.panelMain.add(this.txtField);
+        this.panelMain.add(this.buttonOk);
+        
+        
         
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
