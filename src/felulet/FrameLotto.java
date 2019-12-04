d39f4360c8629b6dc4acf1ece4c292cb1201678e
Diak <Diak@10.4.117.19>
2019-12-04 15:21:05 +0100

    added még 1 button + zsa logika

--- src/felulet/FrameLotto.java
+++ src/felulet/FrameLotto.java
@@ -1,6 +1,7 @@
 
 package felulet;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -14,8 +15,10 @@ public class FrameLotto extends JFrame implements ActionListener{
 
     private JTextField txtField;
     private JButton buttonOk;
+    private JButton buttonRendez;
     private JPanel panelMain;
     private JComboBox comboBoxTipus;
+    
     
     public FrameLotto() {
         initComponents();
@@ -33,23 +36,35 @@ public class FrameLotto extends JFrame implements ActionListener{
         this.txtField.setLocation(20,20);
         this.txtField.setSize(250,20);
         this.txtField.setText("Sajt uwu");
+        this.txtField.setFont(new Font("Arial", Font.PLAIN,20));
 
         this.buttonOk = new JButton();
         this.buttonOk.setText("Sorsol");
         this.buttonOk.setLocation(20,50);
         this.buttonOk.setSize(250,25);
-
+        this.txtField.setFont(new Font("Arial", Font.PLAIN,20));
+        
+        this.buttonRendez = new JButton();
+        this.buttonRendez.setText("Rendez");
+        this.buttonRendez.setLocation(20,50);
+        this.buttonRendez.setSize(250,25);
+        this.buttonRendez.setFont(new Font("Arial", Font.PLAIN,20));
+        
+        
         this.comboBoxTipus = new JComboBox();
         this.comboBoxTipus.addItem("Ötös");
         this.comboBoxTipus.addItem("Hatos");
         this.comboBoxTipus.addItem("Skandi");
         this.comboBoxTipus.setLocation(20,80);
         this.comboBoxTipus.setSize(200,50);
+        this.txtField.setFont(new Font("Arial", Font.PLAIN,20));
         
         
         this.panelMain.add(this.txtField);
         this.panelMain.add(this.buttonOk);
+        this.panelMain.add(this.buttonRendez);
         this.buttonOk.addActionListener(this);
+        this.buttonRendez.addActionListener(this);
         
  
         
